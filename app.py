90f87336d18f4218f3dfe701a90e0bf286ccc295
Rakesh Ray <raycsit@yahoo.com>
2024-09-28 11:14:35 +0530

    code modified

--- app.py
+++ app.py
@@ -1,9 +1,23 @@
 import streamlit as st
+import pandas as pd
+import numpy as np
+import datetime
 
-st.header("_Streamlit_ is :blue[cool] :sunglasses:")
-st.header("This is a header with a divider", divider="gray")
-st.header("These headers have rotating dividers", divider=True)
-st.header("One", divider=True)
-st.header("Two", divider=True)
-st.header("Three", divider=True)
-st.header("Four", divider=True)
+st.header("Cars24 Data :sunglasses:")
+st.header("Bar Chart", divider="gray")
+#st.header("These headers have rotating dividers", divider=True)
+#st.header("One", divider=True)
+#st.header("Two", divider=True)
+#st.header("Three", divider=True)
+#st.header("Four", divider=True)
+df = pd.read_csv('cars24-car-price.csv')
+st.dataframe(df.head(2))
+st.bar_chart(data=df, x='fuel_type',y='mileage')
+
+d = st.date_input("Enter the date", datetime.date(2023,1,2))
+
+#st.text('Datatime is : ')
+#st.show('Datatime is : ')
+#from PIL import Image
+#image = Image.open("sunrise.jpg")
+#st.image(image)
